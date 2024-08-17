cbf86d664b625d88f84ecf9a7e77ae0348c71b81
Dimitrije70 <dimitrijemitrovic145@gmail.com>
2024-08-17 12:42:18 +0200

    Add files via upload

---
+++ TypingGame/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace TypingGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblRight = new System.Windows.Forms.Label();
+            this.lblWrong = new System.Windows.Forms.Label();
+            this.lblWord = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblRight
+            // 
+            this.lblRight.AutoSize = true;
+            this.lblRight.Location = new System.Drawing.Point(36, 35);
+            this.lblRight.Name = "lblRight";
+            this.lblRight.Size = new System.Drawing.Size(84, 20);
+            this.lblRight.TabIndex = 1;
+            this.lblRight.Text = "Correct: 0";
+            // 
+            // lblWrong
+            // 
+            this.lblWrong.AutoSize = true;
+            this.lblWrong.Location = new System.Drawing.Point(639, 35);
+            this.lblWrong.Name = "lblWrong";
+            this.lblWrong.Size = new System.Drawing.Size(94, 20);
+            this.lblWrong.TabIndex = 2;
+            this.lblWrong.Text = "Incorrect: 0";
+            // 
+            // lblWord
+            // 
+            this.lblWord.AutoSize = true;
+            this.lblWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWord.Location = new System.Drawing.Point(298, 253);
+            this.lblWord.Name = "lblWord";
+            this.lblWord.Size = new System.Drawing.Size(143, 55);
+            this.lblWord.TabIndex = 3;
+            this.lblWord.Text = "Word";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(244, 517);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(314, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Press Enter to Check the Text with Word";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(109, 347);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(550, 61);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.checkGame);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(774, 689);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblWord);
+            this.Controls.Add(this.lblWrong);
+            this.Controls.Add(this.lblRight);
+            this.Name = "Form1";
+            this.Text = "Typing Game - By Dimi";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblRight;
+        private System.Windows.Forms.Label lblWrong;
+        private System.Windows.Forms.Label lblWord;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ TypingGame/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TypingGame
+{
+    public partial class Form1 : Form
+    {
+        string[] words = { "internet", "programming", "dimi", "coding", "sky", "banana" };
+
+        Random rnd = new Random();
+
+        int correct = 0;
+        int incorrect = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            lblWord.Text = words[rnd.Next(0, words.Length)];
+        }
+
+        private void checkGame(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (textBox1.Text == lblWord.Text)
+                {
+                    correct++;
+                    lblWord.Text = words[rnd.Next(0, words.Length)];
+                    textBox1.Text = null;
+                }
+
+                else
+                {
+                    incorrect++;
+                    lblWord.Text = words[rnd.Next(0, words.Length)];
+                    textBox1.Text = null;
+                }
+
+                lblRight.Text = "Correct: " + correct;
+                lblWrong.Text = "Incorrect: " + incorrect;
+            }
+        }
+    }
+}
